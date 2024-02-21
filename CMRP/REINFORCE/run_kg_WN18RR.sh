d1d551a326c304427b591bc4f9eb5de075f35fb9
JohnJiang12138 <159504445+JohnJiang12138@users.noreply.github.com>
2024-02-21 17:13:25 +0800

    Delete CMRP directory

--- CMRP/REINFORCE/run_kg_WN18RR.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 python -u main.py --network_type lstm --agent_optim adam --agent_lr 0.0035 \
-    --agent_optim sgd --entropy_coeff 0.0001 --graph_task 'kg' --llm_task 'qa' --datasetname 'WN18RR' --batch_size 64 \
-    --alpha 1.0 --beta 0.2 --gamma 0.1 --alpha1 10.0 --beta1 10 \
-    --LP_model 'transh' --max_epoch 10 --train_times 500
